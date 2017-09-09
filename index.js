f045401afe75da86c0ae6d2b2457eb0f4b7d661d
Kevin Krause <kkrausesc@gmail.com>
2017-09-09 18:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(HELLO) {
-  console.log('HELLO'.toLowerCase)
+function whisper(string) {
+  return string.toLowerCase()
 }
