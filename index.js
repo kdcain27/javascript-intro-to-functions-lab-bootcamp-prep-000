2c9f05e3dc3b0c6f22233b9eec2bc3ea62e0dc76
Kevin Krause <kkrausesc@gmail.com>
2017-09-09 19:30:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGandma(string) {
-  if string === lowercase
-    return "I can't hear you!"
+  if (string === lowercase) {
+    return "I can't hear you!"}
   else if ("string" === uppercase)
     return "YES INDEED!"
   else if ("string" === "I love you, Grandma.")
