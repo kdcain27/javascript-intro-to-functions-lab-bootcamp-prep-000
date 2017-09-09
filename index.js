cb47cfeac1cc2c1798732b9fb4ff5ec78a102592
Kevin Krause <kkrausesc@gmail.com>
2017-09-09 21:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,20 +14,14 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-var uppercase = "HELLO"
 
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = "hello"
-
-lowercase.toLowerCase() === lowercase // true
 
 var love = "I love you, Grandma."
 
 function sayHiToGandma(string) {
-  if (string.toLowerCase) {
+  if (string.toLowerCase()) {
     return "I can't hear you!"}
-  if (string.toUpperCase) {
+  if (string.toUpperCase()) {
     return "YES INDEED!"}
   else if (love){
     return "I love you, too."}
